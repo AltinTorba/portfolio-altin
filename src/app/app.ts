9ba0a94fe6edfedd87596bd30bfd0a3110c2a6c0
AltinTorba <alabto@gmail.com>
2026-01-14 15:14:06 +0100

    Add Imprint/Privacy pages with routing

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/header/header';
 import { Footer } from './shared/footer/footer';
-import { MainContent } from "./main-content/main-content";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer, MainContent],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
