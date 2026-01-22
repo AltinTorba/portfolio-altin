1c9c8784f7767d568f7181b6fdaccc683cf9dcbc
AltinTorba <alabto@gmail.com>
2026-01-22 21:57:52 +0100

    Create Privacy components with German/English translations

--- src/app/contactform/contactform.ts
+++ src/app/contactform/contactform.ts
@@ -7,20 +7,21 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-contactform',
   standalone: true,
-  imports: [FormsModule, TranslateModule, RouterLink],
+  imports: [ FormsModule, TranslateModule, RouterLink ],
   templateUrl: './contactform.html',
   styleUrl: './contactform.scss'
 })
 export class Contactform {
-  
-  constructor(private http: HttpClient) {}
+
+  constructor (private http: HttpClient) { }
 
   contactData = {
     name: "",
     email: "",
     message: "",
     checkbox: false,
-  }
+    company: ""
+  };
 
   nameTouched = {
     name: false,
@@ -29,42 +30,39 @@ export class Contactform {
   };
 
   markAsTouched(field: 'name' | 'email' | 'message') {
-    this.nameTouched[field] = true;
+    this.nameTouched[ field ] = true;
   }
 
   mailTest = false;
 
   post = {
-    endPoint: 'https://altintorba.net/sendMail.php',
-    body: (payload: any) => JSON.stringify(payload),
-    options: {
-      headers: { 'Content-Type': 'text/plain' },
-      responseType: 'text' as const
-    }
+    endPoint: 'https://jetaever8.de/wp-json/contact/v1/send',
   };
 
   onSubmit(form: any) {
-    if (form.submitted && form.valid && !this.mailTest) {
-      this.http.post(
-        this.post.endPoint,
-        this.post.body(this.contactData),
-        this.post.options
-      ).subscribe({
-        next: (response) => {
+    if (form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.contactData).subscribe({
+        next: () => {
           form.reset();
-          this.contactData = { name: "", email: "", message: "", checkbox: false };
-          this.nameTouched = { name: false, email: false, message: false };
-          console.log('Email sent successfully!', response);
+          this.contactData = {
+            name: '',
+            email: '',
+            message: '',
+            checkbox: false,
+            company: ""
+          };
+          this.nameTouched = {
+            name: false,
+            email: false,
+            message: false
+          };
+          console.log('Email sent successfully');
         },
-        error: (error) => {
-          console.error('Sending failed:', error);
+        error: (err) => {
+          console.error('Sending failed:', err);
         }
       });
-    } else if (form.submitted && form.valid && this.mailTest) {
-      console.info('MailTest enabled – no email sent');
-      form.reset();
-      this.contactData = { name: "", email: "", message: "", checkbox: false };
-      this.nameTouched = { name: false, email: false, message: false };
     }
   }
+
 }
