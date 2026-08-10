6f6df409d59b09a092f8f569c2bdf19ec60c3c10
AltinTorba <alabto@gmail.com>
2026-08-10 12:15:10 +0200

    fix: contactform - prevent double submit, migrate to EmailJS, center viza

--- src/app/contactform/contactform.ts
+++ src/app/contactform/contactform.ts
@@ -1,8 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
-import { HttpClient } from '@angular/common/http';
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-contactform',
@@ -12,8 +12,6 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './contactform.scss'
 })
 export class Contactform {
-  private http = inject(HttpClient);
-
 
   contactData = {
     name: "",
@@ -37,15 +35,33 @@ export class Contactform {
 
   submitStatus: 'idle' | 'success' | 'error' = 'idle';
 
-  post = {
-    endPoint: 'https://jetaever8.de/wp-json/contact/v1/send',
+  isSubmitting = false;
+
+  emailjsConfig = {
+    serviceId: 'service_90qxwae',
+    templateId: 'template_tluall3',
+    publicKey: 'hK94NwwkXIftab1dj',
   };
 
   onSubmit(form: NgForm) {
-    if (form.valid && !this.mailTest) {
-      this.http.post(this.post.endPoint, this.contactData).subscribe({
-        next: () => {
+    if (form.valid && !this.mailTest && !this.isSubmitting) {
+      this.isSubmitting = true;
+
+      const templateParams = {
+        name: this.contactData.name,
+        email: this.contactData.email,
+        message: this.contactData.message,
+      };
+
+      emailjs.send(
+        this.emailjsConfig.serviceId,
+        this.emailjsConfig.templateId,
+        templateParams,
+        this.emailjsConfig.publicKey
+      ).then(
+        () => {
           this.submitStatus = 'success';
+          this.isSubmitting = false;
 
           form.reset();
           this.contactData = {
@@ -61,10 +77,12 @@ export class Contactform {
             message: false
           };
         },
-        error: () => {
+        (error) => {
+          console.error('EmailJS error:', error);
           this.submitStatus = 'error';
+          this.isSubmitting = false;
         }
-      });
+      );
     }
   }
 
