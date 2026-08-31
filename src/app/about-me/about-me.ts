131c717c2071f44713017fcfd6e294ce95cb7e7a
AltinTorba <alabto@gmail.com>
2026-08-31 11:17:19 +0200

    refactor: rebuild curve positioning with CSS custom property instead of per-breakpoint media queries

--- src/app/about-me/about-me.ts
+++ src/app/about-me/about-me.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
+import { Component, ElementRef, ViewChild, AfterViewInit, HostListener, inject } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-about-me',
@@ -7,4 +7,25 @@ import { TranslateModule } from '@ngx-translate/core';
   templateUrl: './about-me.html',
   styleUrls: ['./about-me.scss']
 })
-export class AboutMe {}
+export class AboutMe implements AfterViewInit {
+  @ViewChild('photoContainer') photoContainer!: ElementRef<HTMLElement>;
+  private translate = inject(TranslateService);
+
+  ngAfterViewInit(): void {
+    this.updatePhotoBottom();
+    this.translate.onLangChange.subscribe(() => {
+      setTimeout(() => this.updatePhotoBottom(), 0);
+    });
+  }
+
+  @HostListener('window:resize')
+  onResize(): void {
+    this.updatePhotoBottom();
+  }
+
+  private updatePhotoBottom(): void {
+    const rect = this.photoContainer.nativeElement.getBoundingClientRect();
+    const bottomAbsolute = rect.bottom + window.scrollY;
+    document.documentElement.style.setProperty('--photo-bottom', `${bottomAbsolute}px`);
+  }
+}
